75693037370dea22dd8db361d0ac35f4a08d1e42
y-macpro <y-macpro>
2018-04-24 19:42:43 +0800

    实验 二

---
+++ samples/exp1.rb
@@ -0,0 +1,14 @@
+## 实验一： 声音的和谐
+
+
+# 不和谐的声音
+play [:C, :C + 1], release: 5 # do do#
+play [:C, :C + 2], release: 5 # do re
+
+# 和谐的声音
+play [:C, :C + 7], release: 5 # do so 属音
+play [:C, :C + 5], release: 5 # do fa 下属音
+play [:C, :C + 12], release: 5 # do do5 八度音
+
+## puts 2 ** 7
+## puts (3 / 2.0) ** 12
---
+++ samples/exp2.rb
@@ -0,0 +1,26 @@
+## 不同的音色
+
+# 正弦音
+play :C4, amp: 1, release:5
+
+# 合成的声音
+play :C4, amp: 1, release:5
+play :C5, amp: 0.5, release:5
+play :C6, amp: 0.25, release:5
+play :C7, amp: 0.125, release:5
+
+
+
+## 不同的乐器
+
+use_synth :beep
+play :C4, release: 10
+
+use_synth :square
+play :C4, release: 10
+
+use_synth :saw
+play :C4, release: 10
+
+use_synth :piano
+play :C4, release: 10
